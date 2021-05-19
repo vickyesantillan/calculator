2c23ad583da3e7a57710bd0f2502c72cb68ff478
vickyesantillan <vesantillan@live.com>
2021-05-19 17:29:21 -0300

    making a bmi calculator

--- calculator.js
+++ calculator.js
@@ -2,10 +2,14 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true })); //what I need to use body parsser always
+app.use(bodyParser.urlencoded({ extended: true })); //what I need to use body parser always
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html'); //the path to our file wherever we are
+});
+
+app.get('/bmi', function (req, res) {
+  res.sendFile(__dirname + '/bmiCalculator.html');
 });
 
 app.get('/contact', function (req, res) {
@@ -18,6 +22,14 @@ app.post('/', function (req, res) {
   var result = num1 + num2;
 
   res.send('The result is ' + result);
+});
+
+app.post('/bmi', function (req, res) {
+  var height = Number(req.body.height);
+  var weight = Number(req.body.weight);
+  var bmi = weight / (height * height);
+
+  res.send('Your BMI is ' + bmi);
 });
 
 app.listen(3000, function () {
