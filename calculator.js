677d6ad4c770cd65a0c67c00ab6a7360454c689b
vickyesantillan <vesantillan@live.com>
2021-05-19 17:09:24 -0300

    making the calculator to work

--- calculator.js
+++ calculator.js
@@ -1,5 +1,8 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true })); //what I need to use body parsser always
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html'); //the path to our file wherever we are
@@ -10,7 +13,11 @@ app.get('/contact', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-  res.send('dude qie es eso');
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var result = num1 + num2;
+
+  res.send('The result is ' + result);
 });
 
 app.listen(3000, function () {
